b767576b03bfa967a8af041b4a41f5deb70dc88c
andriyanwawan8@gmail.com <andriyanwawan8@gmail.com>
2019-10-28 11:53:18 +0700

    first file

---
+++ latihan1python.py.py
@@ -0,0 +1,16 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 19:28:18) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=input("masukan nilai a:20")
+masukan nilai a:20
+>>> a=input("masukan nilai a:2")
+masukan nilai a:2
+>>> print("hasil penggabungan {1}&{0}=%d".format(20,2) %(20+2))
+hasil penggabungan 2&20=22
+>>> #Konversi nilai variable
+>>> a=int(20)
+>>> a=int(2)
+>>> print("hasil penjumlahan {1}&{0}=%d".format(20,2) %(20+2))
+hasil penjumlahan 2&20=22
+>>> print("hasil pembagian {1}/{0}=%d".format(20,2) %(20/2))
+hasil pembagian 2/20=10
+>>> 
